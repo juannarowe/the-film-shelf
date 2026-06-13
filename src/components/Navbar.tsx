b96519c59aae69fc51efab1e998713d9c0d75a53
juannarowe <juannarowe@gmail.com>
2026-06-14 00:59:14 +0200

    feat: implemented basic navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function navBar() {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/explore">Explore</a></li>
+                <li><a href="/movies">Favorites</a></li>
+            </ul>
+        </nav>
+    )
+}
