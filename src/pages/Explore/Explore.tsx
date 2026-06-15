e104faedb0a44282394955154fa4a0c05f090a5a
juannarowe <juannarowe@gmail.com>
2026-06-15 21:53:37 +0200

    feat: Explore component implemented with movies rendered to the page

--- src/pages/Explore/Explore.tsx
+++ src/pages/Explore/Explore.tsx
@@ -1,25 +1,33 @@
 import { useState, useEffect } from 'react'
 import getMovies from '../../services/TMDB'
 
+interface Movie {
+    id: number
+    title: string
+    genre: string
+    overview: string
+    poster_path: string
+}
+
 export default function Explore() {
     // First I create the state:
-    const [movies, setMovies] = useState([])
+    const [movies, setMovies] = useState<Movie[]>([])
 
-    // Then, call the function to get the movies. When the movies arrive, save the/*  */ in the state.
+    // Then, call the function to get the movies. When the movies arrive, save them in the state.
     useEffect(() => {
         getMovies().then((data) => {
             console.log(data)
             setMovies(data)})
     }, [])
 
+    // Finally, I can render the movies in the component. The map needs a key (movie ID). Alt for accessibility.
     return (
         <div>
-            {/* {movies.map((movie) => (
+            {movies.map(movie => (
                 <div key={movie.id}>
-                    <h2>{movie.title}</h2>
-                    <p>{movie.overview}</p>
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
                 </div>
-            ))} */}
+            ))}
         </div>
-    )
+        )
 }
