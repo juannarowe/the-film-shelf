f8d3c6c5b28b9271cbbdb20c21600f97f75c8349
juannarowe <juannarowe@gmail.com>
2026-06-16 20:58:25 +0200

    feat: finished fetch by movie ID at FilmDetail. Created movie interface at types/movie.ts

--- src/pages/FilmDetail/FilmDetail.tsx
+++ src/pages/FilmDetail/FilmDetail.tsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router"
 import { getMovieById } from "../../services/TMDB"
+import type { Movie } from "../../types/movie"
 
 export default function FilmDetail() {
     const { id } = useParams<{ id: string }>()
 
-    const [movie, setMovie] = useState([])
+    const [movie, setMovie] = useState<Movie | null>(null)
 
     useEffect(() => {
         if (id) {
@@ -13,12 +14,14 @@ export default function FilmDetail() {
         }
     }, [id])
 
-    console.log(movie)
+    if (!movie) return <p>Loading...</p>
+
+    const { poster_path, overview, release_date, original_title, original_language, title, vote_average } = movie
 
     return (
         <div className="">
-            <h1>Film Detail Page</h1>
+            <h1>{title}</h1>
             <p>Discover more about a movie here!</p>
         </div>
     )
-}
+}   
---
+++ src/types/movie.ts
@@ -0,0 +1,11 @@
+export interface Movie {
+    id: number;
+    title: string;
+    genre_ids: number[];
+    overview: string;
+    poster_path: string;
+    release_date: string;
+    original_title: string;
+    original_language: string;
+    vote_average: number;
+}
