15c5387fed1927cb965f930bba1762116a0c0d84
juannarowe <juannarowe@gmail.com>
2026-06-17 13:32:28 +0200

    feat(fetch): applied fetch for missing movie credits and implemented promise.all for it to work better

--- src/pages/FilmDetail/FilmDetail.tsx
+++ src/pages/FilmDetail/FilmDetail.tsx
@@ -1,16 +1,25 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router"
-import { getMovieById } from "../../services/TMDB"
+import { getMovieById, getMovieCredits } from "../../services/TMDB"
 import type { Movie } from "../../types/movie"
+
+const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w500'
 
 export default function FilmDetail() {
     const { id } = useParams<{ id: string }>()
 
     const [movie, setMovie] = useState<Movie | null>(null)
+    const [credits, setCredits] = useState<any | null>(null)
 
     useEffect(() => {
         if (id) {
-            getMovieById(id).then(data => setMovie(data))
+            Promise.all([
+                getMovieById(id),
+                getMovieCredits(id)
+            ]).then(([movieData, creditsData]) => {
+                setMovie(movieData)
+                setCredits(creditsData)
+            })
         }
     }, [id])
 
@@ -20,8 +29,11 @@ export default function FilmDetail() {
 
     return (
         <div className="">
+            <img src={`${IMG_BASE_URL}${poster_path}`} alt={`Poster of ${title}`} />
             <h1>{title}</h1>
-            <p>Discover more about a movie here!</p>
+            <h2>{`Original title: ${original_title}.`}</h2>
+            <h3>{`Release data: ${release_date}`}</h3>
+            <p>{overview}</p>
         </div>
     )
 }   
--- src/services/TMDB.ts
+++ src/services/TMDB.ts
@@ -1,14 +1,15 @@
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 const BASE_URL = "https://api.themoviedb.org/3/discover/movie?api_key="
 const MOVIE_BASE_URL = "https://api.themoviedb.org/3/movie/"
+const MOVIE_CREDITS_BASE_URL = "https://api.themoviedb.org/3/credit/"
 
 export async function getMovies() {
     try {
         const response = await fetch(`${BASE_URL}${API_KEY}`)
         const data = await response.json()
         return data.results
-    } catch (err) {
-        throw new Error("Error fetching the data.")
+    } catch (error) {
+        throw new Error("Error fetching the list of movies.")
     }
 }
 
@@ -17,7 +18,18 @@ export async function getMovieById(id: string) {
         const response = await fetch(`${MOVIE_BASE_URL}${id}?api_key=${API_KEY}`)
         const data = await response.json()
         return data
-    } catch (err) {
-        throw new Error("Error fetching the data.")
+    } catch (error) {
+        throw new Error("Error fetching the movie details.")
+    }
+}
+
+export async function getMovieCredits(id: string) {
+    try {
+        const response = await fetch(`${MOVIE_CREDITS_BASE_URL}${id}?api_key=${API_KEY}`)
+        const data = await response.json()
+        console.log(data)
+        return data
+    } catch (error) {
+        throw new Error("Error fetching the movie credits.")
     }
 }
