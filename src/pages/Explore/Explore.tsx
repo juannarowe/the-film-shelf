d304e236da7507bfed4cf8a775ebba01070a1722
juannarowe <juannarowe@gmail.com>
2026-06-18 10:35:35 +0200

    feat: implemented new function searchMovies at TMDB.ts

--- src/pages/Explore/Explore.tsx
+++ src/pages/Explore/Explore.tsx
@@ -1,32 +1,58 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { getMovies } from '../../services/TMDB'
 import { Link } from 'react-router'
 
-interface Movie {
-    id: number
-    title: string
-    genre: string
-    overview: string
-    poster_path: string
+export interface Movie {
+    id: number;
+    title: string;
+    genre_ids: number[];
+    overview: string;
+    poster_path: string;
+    release_date: string;
+    original_title: string;
+    original_language: string;
+    vote_average: number;
 }
 
 const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w500'
 
 export default function Explore() {
     const [movies, setMovies] = useState<Movie[]>([])
+    const [searchTerm, setSearchTerm] = useState<string>('')
+
+    const searchRef = useRef<HTMLInputElement>(null)
+
+    function handleSearch() {
+        const term = searchRef.current?.value.toLowerCase() || ''
+        setSearchTerm(term)
+    }
 
     useEffect(() => {
         getMovies().then((data) => {
-            setMovies(data)})
+            setMovies(data)
+        })
     }, [])
 
+    const filteredMovies = movies.filter(movie =>
+        movie.title.toLowerCase().includes(searchTerm)
+    )
+
     return (
-        <div>
-            {movies.map(movie => (
-                <Link key={movie.id} to={`/film-detail/${movie.id}`}>
-                    <img src={`${IMG_BASE_URL}${movie.poster_path}`} alt={movie.title} />
-                </Link>
-            ))}
-        </div>
-        )
+        <>
+            <div>
+                <div>
+                    <input ref={searchRef} type="search" placeholder="Search" />
+                    <button onClick={handleSearch}>Search</button>
+                </div>
+            </div>
+            <div>
+                {filteredMovies.map(movie => (
+                    <Link key={movie.id} to={`/film-detail/${movie.id}`}>
+                        <img src={`${IMG_BASE_URL}${movie.poster_path}`} alt={movie.title} />
+                    </Link>
+                ))}
+            </div>
+        </>
+
+    )
 }
--- src/services/TMDB.ts
+++ src/services/TMDB.ts
@@ -1,6 +1,7 @@
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 const BASE_URL = "https://api.themoviedb.org/3/discover/movie?api_key="
 const MOVIE_BASE_URL = "https://api.themoviedb.org/3/movie/"
+const SEARCH_BASE_URL = "https://api.themoviedb.org/3/search/movie?api_key="
 
 export async function getMovies() {
     try {
@@ -29,5 +30,15 @@ export async function getMovieCredits(id: string) {
         return data
     } catch (error) {
         throw new Error("Error fetching the movie credits.")
+    }
+}
+
+export async function searchMovies(query: string) {
+    try {
+        const response = await fetch(`${SEARCH_BASE_URL}${API_KEY}&query=${encodeURIComponent(query)}`)
+        const data = await response.json()
+        return data.results
+    } catch (error) {
+        throw new Error("Error searching the movie.")
     }
 }
