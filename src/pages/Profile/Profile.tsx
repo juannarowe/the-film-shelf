782076410045d566869f4796afa12d6471cd0d53
juannarowe <juannarowe@gmail.com>
2026-06-18 20:42:46 +0200

    feat(auth): implemented logout button

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,8 +1,14 @@
 import { useAuth } from "../../context/authContext"
-import { Navigate } from "react-router"
+import { Navigate, useNavigate } from "react-router"
 
 export default function Profile() {
-    const { user } = useAuth()
+    const { user, logout } = useAuth()
+    const navigate = useNavigate()
+
+    function handleLogout() {
+        logout()
+        navigate('/login')
+    }
 
     if (!user) {
         return <Navigate to="/login" />
@@ -13,6 +19,9 @@ export default function Profile() {
             <h1 className="text-2xl font-bold">{user.displayName}</h1>
             {user.photoURL && <img src={user.photoURL} alt={user.displayName ?? 'Profile Picture'} className="w-24 h-24 rounded-full object-cover border-2 border-white" />}
             <p className="text-gray-400">{user.email}</p>
+            <button onClick={handleLogout} className="cursor-pointer bg-white text-gray-900 font-semibold px-6 py-3 rounded-lg hover:bg-gray-100 shadow-md transition-colors">
+                Logout
+            </button>
         </div>
     )
 }
